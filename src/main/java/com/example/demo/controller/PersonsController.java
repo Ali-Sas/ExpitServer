59193a27d7211435d3cf56a8031d710db07ac3dc
Alexandr Daiev <s.daew@yandex.ru>
2022-05-29 00:48:18 +0300

    make it better

--- src/main/java/com/example/demo/controller/PersonsController.java
+++ src/main/java/com/example/demo/controller/PersonsController.java
@@ -3,12 +3,8 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
