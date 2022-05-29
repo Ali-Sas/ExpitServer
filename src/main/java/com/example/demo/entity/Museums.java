73d87ace4df273b03d4498462b44ba9f3f1e91f5
Alexandr Daiev <s.daew@yandex.ru>
2022-05-29 16:25:42 +0300

    make it better

--- src/main/java/com/example/demo/entity/Museums.java
+++ src/main/java/com/example/demo/entity/Museums.java
@@ -1,54 +1,54 @@
 package com.example.demo.entity;
 
 public class Museums {
-	public Integer id;
-	public String nameMuseum, countryMuseum, imageMuseum, coordsMuseum, infoMuseum;
-	
+    public Integer id;
+    public String nameMuseum, countryMuseum, imageMuseum, coordsMuseum, infoMuseum;
+
     public Integer getId() {
         return id;
     }
- 
+
     public void setId(Integer id) {
         this.id = id;
     }
-    
+
     public void setNameMuseum(String name) {
         this.nameMuseum = name;
     }
-    
-    public String nameMuseum() {
+
+    public String getNameMuseum() {
         return nameMuseum;
     }
-    
+
     public void setCountryMuseum(String country) {
         this.countryMuseum = country;
     }
-    
-    public String countryMuseum() {
+
+    public String getCountryMuseum() {
         return countryMuseum;
     }
-    
+
     public void setImageMuseum(String image) {
         this.imageMuseum = image;
     }
-    
-    public String imageMuseum() {
+
+    public String getImageMuseum() {
         return imageMuseum;
     }
-    
+
     public void setCoordsMuseum(String coords) {
         this.coordsMuseum = coords;
     }
-    
-    public String coordsMuseum() {
+
+    public String getCoordsMuseum() {
         return coordsMuseum;
     }
-    
+
     public void setInfoMuseum(String info) {
         this.infoMuseum = info;
     }
-    
-    public String infoMuseum() {
+
+    public String getInfoMuseum() {
         return infoMuseum;
     }
 
